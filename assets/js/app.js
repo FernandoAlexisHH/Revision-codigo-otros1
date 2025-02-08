7f3b4c8879951a54b11413f8a429e98bd363d4cf
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-02-08 17:17:09 -0600

    "fix:revisando el codigo para que funcióne"

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,46 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = "https://api.github.com";
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
+// Coloque nombres de variables más descriptivos
+const $name = document.querySelector(".name"); // Usando "." para seleccionar por clase
+const $blog = document.querySelector(".blog");
+const $location = document.querySelector(".location");
+const $imagenAvatar = document.querySelector(".avatar");
+const $seguidores = document.querySelector(".seguidores");
 
 function handleError(err) {
-  console.log('OH NO!');
+  console.log("OH NO!");
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $name.textContent = `${err}`;
 }
 
-displayUser('stolinski').catch(handleError);
+// Agregue async porque es una función asincrónica
+async function displayUser(username) {
+  //Utilice un try catch para manejar los errores ante una posible excepción del servidor
+  try {
+    $name.textContent = "cargando...";
+    const response = await fetch(`${usersEndpoint}/${username}`);
+
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: Usuario no encontrado`);
+    }
+    const usuario = await response.json(); // Convertir la respuesta en objeto JSON
+
+    console.log(usuario);
+
+    // Verificar si existen los valores antes de asignarlos
+    $name.textContent = usuario.name || "No disponible";
+    $blog.textContent = usuario.blog || "No disponible";
+    $location.textContent = usuario.location || "No disponible";
+    $imagenAvatar.src = usuario.avatar_url || "#";
+    $seguidores.textContent =
+      `Seguidores: ${usuario.followers}` || "No tengo seguidores";
+  } catch (err) {
+    handleError(
+      err
+    ); /*Utilice una funcion aparte para dividir responsabilidades*/
+  }
+}
+
+// Llamada a la función con manejo de errores
+displayUser("stolinski");
